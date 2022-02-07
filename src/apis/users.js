cb6e678348c1a220eda08d0f51a6bf88fd597903
Martin-no1tw <kljhger7728@gmail.com>
2022-02-08 00:14:40 +0800

    feat:add restaurant like and unlike

--- src/apis/users.js
+++ src/apis/users.js
@@ -4,11 +4,19 @@ const getToken = () => localStorage.getItem('token')
 
 
 export default {
-  addFavorite(restaurantId) {
+  addFavorite({ restaurantId }) {
     return apiHelper.post(`/favorite/${restaurantId}`, null, { headers: { Authorization: `Bearer ${getToken()}` } })
   },
   deleteFavorite({ restaurantId }) {
     return apiHelper.delete(`/favorite/${restaurantId}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  addLike({ restaurantId }) {
+    return apiHelper.post(`/like/${restaurantId}`, null, { headers: { Authorization: `Bearer ${getToken()}` } })
+  },
+  deleteLike({ restaurantId }) {
+    return apiHelper.delete(`/like/${restaurantId}`, {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
   }
