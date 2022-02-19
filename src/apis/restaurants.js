f455ef2da49f45811127e3770d49400d0593a982
Martin-no1tw <kljhger7728@gmail.com>
2022-02-20 03:01:48 +0800

    feat:add restaurant detail

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -3,6 +3,11 @@ import { apiHelper } from "./../utils/apiHelpers";
 const getToken = () => localStorage.getItem('token')
 
 export default {
+  getRestaurant({ restaurantId }) {
+    return apiHelper.get(`/restaurants/${restaurantId}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
   getRestaurants({ page, categoryId }) {
     const searchParams = new URLSearchParams({ page, categoryId })
     return apiHelper.get(`/restaurants?${searchParams.toString()}`, { headers: { Authorization: `Bearer ${getToken()}` } })
--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -21,103 +21,9 @@
 import RestaurantDetail from "./../components/RestaurantDetail";
 import RestaurantComments from "./../components/RestaurantComments";
 import CreateComment from "./../components/CreateComment";
-
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: "Mr. Santa Willms",
-    tel: "772-157-0829 x24274",
-    address: "62481 Rippin Summit",
-    opening_hours: "08:00",
-    description:
-      "Numquam eaque vero. Alias quam dolorum et voluptatem officiis. Voluptatibus architecto voluptatem doloribus.",
-    image:
-      "https://loremflickr.com/320/240/restaurant,food/?random=33.197567533362694",
-    viewCounts: 1,
-    createdAt: "2022-01-30T18:12:28.000Z",
-    updatedAt: "2022-02-05T07:09:07.853Z",
-    CategoryId: 2,
-    Category: {
-      id: 2,
-      name: "日本料理",
-      createdAt: "2022-01-30T18:12:28.000Z",
-      updatedAt: "2022-01-30T18:12:28.000Z",
-    },
-    FavoritedUsers: [],
-    LikedUsers: [],
-    Comments: [
-      {
-        id: 101,
-        text: "Culpa est culpa sapiente tempora nostrum et.",
-        UserId: 1,
-        RestaurantId: 1,
-        createdAt: "2022-01-30T18:12:28.000Z",
-        updatedAt: "2022-01-30T18:12:28.000Z",
-        User: {
-          id: 1,
-          name: "root",
-          email: "root@example.com",
-          password:
-            "$2a$10$rDvRLt.QeA29/1qCWZXNS.M6MoqFKu25RpRRvV9mClEyJCvKCtiYm",
-          isAdmin: true,
-          image: null,
-          createdAt: "2022-01-30T18:12:28.000Z",
-          updatedAt: "2022-01-30T18:12:28.000Z",
-        },
-      },
-      {
-        id: 51,
-        text: "Minus nostrum dolor quidem.",
-        UserId: 2,
-        RestaurantId: 1,
-        createdAt: "2022-01-30T18:12:28.000Z",
-        updatedAt: "2022-01-30T18:12:28.000Z",
-        User: {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$qtSCyHQddj10rkq0O.xNJOSpUxOOexpQ1LScGpyIa21y.P0mvEZm6",
-          isAdmin: false,
-          image: null,
-          createdAt: "2022-01-30T18:12:28.000Z",
-          updatedAt: "2022-01-30T18:12:28.000Z",
-        },
-      },
-      {
-        id: 1,
-        text: "Eos ut praesentium hic voluptatem et qui.",
-        UserId: 2,
-        RestaurantId: 1,
-        createdAt: "2022-01-30T18:12:28.000Z",
-        updatedAt: "2022-01-30T18:12:28.000Z",
-        User: {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$qtSCyHQddj10rkq0O.xNJOSpUxOOexpQ1LScGpyIa21y.P0mvEZm6",
-          isAdmin: false,
-          image: null,
-          createdAt: "2022-01-30T18:12:28.000Z",
-          updatedAt: "2022-01-30T18:12:28.000Z",
-        },
-      },
-    ],
-  },
-  isFavorited: false,
-  isLiked: false,
-};
-const dummyUser = {
-  currentUser: {
-    id: 1,
-    name: "管理者",
-    email: "root@example.com",
-    image: "https://i.pravatar.cc/300",
-    isAdmin: true,
-  },
-  isAuthenticated: true,
-};
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/apiHelpers";
+import { mapState } from "vuex";
 
 export default {
   name: "restaurant",
@@ -140,41 +46,65 @@ export default {
         isFavorited: false,
         isLiked: false,
       },
-      currentUser: dummyUser.currentUser,
       restaurantComments: [],
     };
+  },
+  computed: {
+    ...mapState(["currentUser"]),
+  },
+  watch: {
+    initialRestaurant(newValue) {
+      this.restaurant = {
+        ...this.restaurant,
+        ...newValue,
+      };
+    },
   },
   created() {
     const { id: restaurantId } = this.$route.params;
     this.fetchRestaurant(restaurantId);
   },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log("fetchRestaurant id: ", restaurantId);
-      const { restaurant, isFavorited, isLiked } = dummyData;
-      const {
-        id,
-        name,
-        Category,
-        image,
-        opening_hours: openingHours,
-        tel,
-        address,
-        description,
-      } = restaurant;
-      this.restaurant = {
-        id,
-        name,
-        categoryName: Category ? Category.name : "未分類",
-        image,
-        openingHours,
-        tel,
-        address,
-        description,
-        isFavorited,
-        isLiked,
-      };
-      this.restaurantComments = dummyData.restaurant.Comments;
+    async fetchRestaurant(restaurantId) {
+      try {
+        // STEP 4: 透過 restaurantsAPI 取得餐廳資訊
+        const { data } = await restaurantsAPI.getRestaurant({ restaurantId });
+
+        // STEP 5: 透過 restaurantsAPI 取得餐廳資訊
+        const { restaurant, isFavorited, isLiked } = data;
+        const {
+          id,
+          name,
+          Category,
+          image,
+          opening_hours: openingHours,
+          tel,
+          address,
+          description,
+          Comments,
+        } = restaurant;
+
+        this.restaurant = {
+          id,
+          name,
+          categoryName: Category ? Category.name : "未分類",
+          image,
+          openingHours,
+          tel,
+          address,
+          description,
+          isFavorited,
+          isLiked,
+        };
+
+        this.restaurantComments = Comments;
+      } catch (error) {
+        // STEP 6: 透過 restaurantsAPI 取得餐廳資訊
+        Toast.fire({
+          icon: "error",
+          title: "無法取得餐廳資料，請稍後再試",
+        });
+      }
     },
     afterDeleteComment(commentId) {
       // 以 filter 保留未被選擇的 comment.id
@@ -194,6 +124,11 @@ export default {
         text,
         createdAt: new Date(),
       });
+    },
+    beforeRouteUpdate(to, from, next) {
+      const { id: restaurantId } = to.params;
+      this.fetchRestaurant(restaurantId);
+      next();
     },
   },
 };
